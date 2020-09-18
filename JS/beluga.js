52ac79ab01406c71c3b7f0f9645c8a733ae6106f
Victor Beccare <vbeccare@redventures.com>
2020-09-18 17:06:18 -0300

    Array, temporizador, math e date

--- JS/beluga.js
+++ JS/beluga.js
@@ -3,11 +3,18 @@ function calcularMedia(){
     var total = 0;
     var qtd = arguments.length;
     var x = 0;
-    //while(arguments[x]){
     while(typeof arguments[x] === 'number'){
         total += arguments[x];
         x++;
     }                    
 
     return (total / qtd).toFixed(2);
+}
+
+function sortear(x){
+    var x = x || 1;
+    var sorteado = Math.random()*x;
+    sorteado = Math.floor(sorteado);
+    return sorteado;
+
 }
